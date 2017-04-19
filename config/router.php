1fcbcf7ce0d7b562a4f0413fad19b389bc7b0d35
Karasev Kirill <karasev.kirill.a@gmail.com>
2017-04-19 20:44:25 +1000

    fix

--- config/router.php
+++ config/router.php
@@ -1,11 +1,7 @@
 <?php
     return [
-
         'baseAction' => 'index',
-
         'baseController' => 'contact',
-
         'notFound' => 'contact/not-found',
-
         'routes' => '^(?<controller>[a-z-]+)?/?(?<action>[a-z-]+)?/?(?<parameter>[a-z0-9/-]+)?$',
     ];
